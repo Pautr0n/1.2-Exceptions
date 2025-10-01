332352d80a147d12bcb1a20d2e9400a8d00b487e
PauG <paugaston@hotmail.com>
2025-10-01 10:15:58 +0200

    Product.java class created

---
+++ src/nivell1/Product.java
@@ -0,0 +1,24 @@
+package nivell1;
+
+class Product {
+
+    private String name;
+    private double price;
+
+    Product (String name, double price){
+
+        this.name = name;
+        this.price = price;
+        IO.println("New product " + this.name + " created, price: " + this.price);
+
+    }
+
+    protected double getPrice(){
+        return price;
+    }
+
+    protected String getName(){
+        return name;
+    }
+
+}
